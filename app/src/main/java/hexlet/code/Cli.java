19e5dab1cc8a0e9074b30b24d94c6b465ed5775a
unknown <reatin@yandex.ru>
2025-03-27 11:33:56 +0300

    added an exclamation mark in cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -8,7 +8,7 @@ public class Cli {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         userName = mainScanner.next();
-        System.out.println("Hello, " + userName);
+        System.out.println("Hello, " + userName + "!");
     }
     public static String getName() {
         return userName;
